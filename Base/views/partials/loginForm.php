8784ad1d80e1ede1331e7d3937327d490070337b
Masy Jordan <masyjordan@gmail.com>
2024-06-25 11:21:06 +0200

    update css/forms

---
+++ Base/views/partials/loginForm.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<header>
+        <img src="../../public/img/pokemon-logo.png" alt="Pokemon Logo">
+        <nav>
+            <ul>
+                <li><a href="#">Pokemon</a></li>
+                <li><a href="#" class="active">My Account</a></li>
+                <li><a href="#">Register</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="login-container">
+            <h2>My Account</h2>
+            <form>
+                <label for="email">Email*</label>
+                <input type="email" id="email" name="email" placeholder="email@yourservice.com" required>
+                <label for="password">Password*</label>
+                <input type="password" id="password" name="password" required>
+                <button type="submit">Connexion</button>
+                <br><br>
+                <p>No account? <a class="Create_One" href="registerFrom.php">Create one!</a></p>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
---
+++ Base/views/partials/registerFrom.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokemon Register</title>
+    <link rel="stylesheet" href="../../assets/css/style.css">
+</head>
+<body>
+    <header>
+        <img src="../../public/img/pokemon-logo.png" alt="Pokemon Logo">
+        <nav>
+            <ul>
+                <li><a href="#">Pokemon</a></li>
+                <li><a href="#">My Account</a></li>
+                <li><a href="#" class="active">Register</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="register-container">
+            <h2>Register</h2>
+            <form>
+            <label for="text">Lastname</label>
+            <input type="lastname" id="lastname" name="lastname"  required>
+            <label for="text">Firstname</label>
+            <input type="firstname" id="firstname" name="firstname" required>
+                <label for="email">Email*</label>
+                <input type="email" id="email" name="email" placeholder="email@yourservice.com" required>
+                <label for="password">Password*</label>
+                <input type="password" id="password" name="password" required>
+                <label for="password-verification">Password Verification*</label>
+                <input type="password" id="password-verification" name="password-verification" required>
+                <button type="submit">New account</button>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
