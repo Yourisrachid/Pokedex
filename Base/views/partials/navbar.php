1265106597bb8bb61a5ad2e9eb7345c1e3df3a9d
Masy Jordan <masyjordan@gmail.com>
2024-06-25 09:28:26 +0200

    update navbar/jordan

--- Base/views/partials/navbar.php
+++ Base/views/partials/navbar.php
@@ -3,51 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css">
     <title>Pokémon NavBar</title>
-    <style>
-        body {
-            margin: 0;
-            font-family: Arial, sans-serif;
-        }
-        .navbar {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding: 10px 20px;
-            background-color: #fff;
-            border-bottom: 1px solid #ddd;
-            transition: background-color 0.3s;
-        }
-        .navbar.dark {
-            background-color: #333;
-            color: #fff;
-        }
-        .navbar img {
-            height: 50px;
-        }
-        .navbar .left-section, .navbar .right-section {
-            display: flex;
-            align-items: center;
-        }
-        .navbar .toggle-darkmode {
-            margin-right: 20px;
-            cursor: pointer;
-        }
-        .navbar input[type="text"] {
-            padding: 5px;
-            margin-right: 20px;
-        }
-        .navbar .profile {
-            width: 40px;
-            height: 40px;
-            border-radius: 50%;
-            background-image: url('../../public/img/man1.png');
-            background-size: cover;
-        }
-        .navbar.dark .profile {
-            background-color: #555;
-        }
-    </style>
 </head>
 <body>
     <nav class="navbar">
